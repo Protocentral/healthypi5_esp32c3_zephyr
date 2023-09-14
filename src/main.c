ca49c71b7008087fa10090a06fa40f0c24a9f4e7
Ashwin K Whitchurch <ashwin@protocentral.com>
2023-09-14 19:46:56 +0530

    Temp commit

--- src/main.c
+++ src/main.c
@@ -4,13 +4,6 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/uart.h>
 
-#include <zephyr/bluetooth/bluetooth.h>
-#include <zephyr/bluetooth/hci.h>
-#include <zephyr/bluetooth/conn.h>
-#include <zephyr/bluetooth/uuid.h>
-#include <zephyr/bluetooth/gatt.h>
-#include <zephyr/bluetooth/services/bas.h>
-#include <zephyr/bluetooth/services/hrs.h>
 #include <zephyr/sys/ring_buffer.h>
 
 #include <zephyr/fs/fs.h>
