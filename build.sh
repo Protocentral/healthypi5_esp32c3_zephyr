4e40c2924749e699cec81e8eade77ddba01985dd
Ashwin <ashwin@protocentral.com>
2023-09-15 10:14:49 +0530

    Updated board def

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 source ~/zephyrproject/zephyr/zephyr-env.sh
-west build -p auto -b healthypi5_esp32c3 . -- -DBOARD_ROOT=.
+#west build -p auto -b healthypi5_esp32c3 . -- -DBOARD_ROOT=.
+
+
+west build -p auto -b healthypi5_esp32c3 --sysbuild . -- -DBOARD_ROOT=/Users/akw/Documents/GitHub/healthypi5_esp32c3_zephyr
+
+#west build -p auto -b xiao_esp32c3 --sysbuild . 
